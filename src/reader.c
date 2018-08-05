1dc3f7dbe6f6100b9142e311fe5d7d55e15c22ba
Akim Demaille <akim.demaille@gmail.com>
2018-08-05 16:29:00 +0200

    warnings: address -Wnull-dereference in reader.c
    Based on a patch by David Michael.
    http://lists.gnu.org/archive/html/bison-patches/2018-07/msg00000.html
    
    * src/reader.c (find_start): New, extracted from...
    (check_and_convert_grammar): here.

--- src/reader.c
+++ src/reader.c
@@ -704,6 +704,26 @@ prepare_percent_define_front_end_variables (void)
   }
 }
 
+/* Find the first LHS which is not a dummy.  */
+
+static symbol *
+find_start_symbol (void)
+{
+  symbol_list *res = grammar;
+  for (;
+       res && symbol_is_dummy (res->content.sym);
+       res = res->next)
+    {
+      for (res = res->next;
+           res && res->content.sym;
+           res = res->next)
+        continue;
+      assume (res);
+    }
+  assume (res);
+  return res->content.sym;
+}
+
 
 /*-------------------------------------------------------------.
 | Check the grammar that has just been read, and convert it to |
@@ -733,19 +753,8 @@ check_and_convert_grammar (void)
   /* Find the start symbol if no %start.  */
   if (!start_flag)
     {
-      symbol_list *node;
-      for (node = grammar;
-           node != NULL && symbol_is_dummy (node->content.sym);
-           node = node->next)
-        {
-          for (node = node->next;
-               node != NULL && node->content.sym != NULL;
-               node = node->next)
-            ;
-        }
-      aver (node != NULL);
-      grammar_start_symbol_set (node->content.sym,
-                                node->content.sym->location);
+      symbol *start = find_start_symbol ();
+      grammar_start_symbol_set (start, start->location);
     }
 
   /* Insert the initial rule, whose line is that of the first rule
